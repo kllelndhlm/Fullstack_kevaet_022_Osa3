256dec63453acae08c5ee15eda63f5b1c6c0dfb0
Kalle Lindholm <kalle.lindholm@helsinki.fi>
2022-02-13 04:03:58 +0200

    Tehtaevaet 3.2-5

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Fullstack-kurssi kevät 2022 Osa 3
+
+Tehty:
+3.1
+3.2
+3.3
+3.4
+3.5
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
 let persons = [
   {
@@ -22,10 +23,74 @@ let persons = [
     number: "39-23-6423122",
   }
 ]
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' })
-  response.end(JSON.stringify(persons))
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</h1>')
 })
+
+
+const generateId = () => {
+  min = 10
+  max= 1000000000
+  return Math.random() * (max - min) + min
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+  if (!body.number) {
+    return response.status(400).json({ 
+      error: 'number missing' 
+    })
+  }
+
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+
+app.get('/info', (req,res) => {
+  res.send(`<p>Phonebook has info for ${persons.length} people.</p><p>${new Date()}</p>`);
+});
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.delete('/api/person/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+app.get('/api/person/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
